1b7fa66928fb84f88e542d2497ff576ab6419c8f
J3SKO <jesko@j3sko.de>
2020-12-19 16:31:19 +0100

    Setup express & ejs

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
-const helloMessage: string = 'Hello World!';
-console.log(helloMessage);
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('index', { message: 'Welcome to WorldEditLab!' });
+});
+
+app.listen(8080, () => {
+  console.log('App is listening on port 8080!');
+});
