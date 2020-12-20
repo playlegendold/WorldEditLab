25467ed45afdaea971cb2efd9f4bd450cfe5e9da
J3SKO <jesko@j3sko.de>
2020-12-20 15:35:26 +0100

    Updated user model

--- src/shared/models/user.ts
+++ src/shared/models/user.ts
@@ -1,4 +1,5 @@
 import { DataTypes, Model, Sequelize } from 'sequelize';
+import Role from './role';
 
 export class User extends Model {}
 
@@ -6,10 +7,16 @@ export const initUser = (sequelize: Sequelize) => {
   User.init({
     name: {
       type: DataTypes.STRING,
+      unique: true,
       allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+    },
+    role: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: Role.GUEST,
     },
   }, {
     sequelize,
