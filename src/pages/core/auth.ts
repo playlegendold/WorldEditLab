2fc022648067593ee53c2506986ea3ee06a9490f
J3SKO <jesko@j3sko.de>
2020-12-20 20:03:21 +0100

    Renamed sign in to login

--- src/pages/core/auth.ts
+++ src/pages/core/auth.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import { buildDefaultResponse } from '../../shared/response';
 
-export const handleSignInRequest = (req: Request, res: Response) => {
+export const handleLoginRequest = (req: Request, res: Response) => {
   res.redirect('/');
 };
 
-export const handleSignInView = (req: Request, res: Response) => {
+export const handleLoginView = (req: Request, res: Response) => {
   const responseData = buildDefaultResponse(req);
-  res.render('sign-in', responseData);
+  res.render('login', responseData);
 };
--- src/pages/core/index.ts
+++ src/pages/core/index.ts
@@ -1,13 +1,13 @@
 import { Application } from 'express';
 import passport from 'passport';
-import { handleSignInRequest, handleSignInView } from './auth';
+import { handleLoginRequest, handleLoginView } from './auth';
 import { LOCAL_SQLITE_STRATEGY } from '../../shared/auth/strategies/local';
 import { handleHomeView } from './home';
 
 const init = (app: Application) => {
   app.get('/', handleHomeView);
-  app.get('/sign-in', handleSignInView);
-  app.post('/sign-in', passport.authenticate(LOCAL_SQLITE_STRATEGY), handleSignInRequest);
+  app.get('/login', handleLoginView);
+  app.post('/login', passport.authenticate(LOCAL_SQLITE_STRATEGY), handleLoginRequest);
 };
 
 export default init;
